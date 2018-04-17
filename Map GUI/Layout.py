8d7faafc7102e8efd222693c15318f8b84844463
ACncpcn <concepcionalex18@yahoo.com>
2018-04-16 21:31:39 -0400

    Add WiFind name to window

--- Map GUI/Layout.py
+++ Map GUI/Layout.py
@@ -41,6 +41,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "WiFind"))
         self.StartToggle.setText(_translate("MainWindow", "Start/Stop"))
 
